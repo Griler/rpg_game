fa1e8e42867cf4b61da0bc7dc50b26c93c31386e
BuildTools <unconfigured@null.spigotmc.org>
2023-10-29 20:50:27 +0700

    update HeathBarSystemUI

--- game/Assets/script/Enemy/EnemyManager.cs
+++ game/Assets/script/Enemy/EnemyManager.cs
@@ -5,31 +5,43 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class EnemyManager : MonoBehaviour,IDamageable
+public class EnemyManager : MonoBehaviour, IDamageable, IHeathSystemUi
 {
     public HitBoxArea _hitBoxArea;
     public EnemyTemplate enemy_Template;
+    public Image heathBar;
+
     //public Text name;
+
+    #region Status Enemy
 
     SpriteRenderer sprite;
     public Animator _animator;
     public int attackDamge;
     public int currentHeath;
     public float speed;
-
     public float time = 0.75f;
 
-    //check ground varriable
+    #endregion
+
+    #region check ground varriable
+
     public float castDistane = 0.25f;
     public Vector2 boxSize = new Vector2(0.3f, 0.1f);
     public LayerMask groundLayer;
+
+    #endregion
+
+    #region stringAnimator
 
     private static readonly int IsDie = Animator.StringToHash("isDie");
     private static readonly int IsHurt = Animator.StringToHash("isHurt");
     private static readonly int IsWalk = Animator.StringToHash("isWalk");
 
+    #endregion
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         _animator = GetComponent<Animator>();
         sprite = GetComponent<SpriteRenderer>();
@@ -40,7 +52,7 @@ public class EnemyManager : MonoBehaviour,IDamageable
         speed = enemy_Template.speed;
     }
 
-     void Update()
+    void Update()
     {
         Attack();
     }
@@ -49,38 +61,46 @@ public class EnemyManager : MonoBehaviour,IDamageable
     {
         if (_hitBoxArea.targets.Count > 0)
         {
-            _animator.SetBool("canAttack",true);
-            _animator.SetBool("lockVelocity",true);
+            _animator.SetBool("canAttack", true);
+            _animator.SetBool("lockVelocity", true);
         }
         else
         {
-            _animator.SetBool("canAttack",false);
-           _animator.SetBool("lockVelocity",false);
-
+            _animator.SetBool("canAttack", false);
+            _animator.SetBool("lockVelocity", false);
         }
     }
-    
+
     public void Damage(int damage)
     {
         currentHeath -= damage;
+        displayHeath();
         if (currentHeath > 0)
         {
             this.GetComponent<EnemyPatrolling>().isHurt(true);
             _animator.SetTrigger(IsHurt);
-            _animator.SetBool(IsWalk,false);
+            _animator.SetBool(IsWalk, false);
             return;
         }
+
         Die();
     }
 
+    public void displayHeath()
+    {
+        float heathRatio = (float)currentHeath / (float)enemy_Template.maxHeath;
+        heathBar.fillAmount = Mathf.Clamp(heathRatio, 0f, 1f);
+        Debug.Log(Mathf.Clamp(heathRatio, 0f, 1f));
+    }
     public int getDamage()
     {
         return attackDamge;
     }
+
     void Die()
     {
         _animator.SetBool(IsDie, true);
-        _animator.SetBool(IsWalk,false);
+        _animator.SetBool(IsWalk, false);
         StartCoroutine(setTimeSetActiveFalse(time));
     }
 
@@ -96,7 +116,7 @@ public class EnemyManager : MonoBehaviour,IDamageable
             return false;
         }
     }
-    
+
     private IEnumerator setTimeSetActiveFalse(float timer)
     {
         yield return new WaitForSeconds(timer);
